24df3905273f104b23915a354b8e39f6d374f719
xiongchao <xiongchao@zafh.com.cn>
2018-09-30 09:42:09 +0800

    spring data jpa datasource config2 - 2018年9月30日09:41:58

--- src/main/java/practice/springboot/config/DataSourceConfig.java
+++ src/main/java/practice/springboot/config/DataSourceConfig.java
@@ -1,6 +1,7 @@
 package practice.springboot.config;
 
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
@@ -21,19 +22,55 @@ public class DataSourceConfig {
         System.out.println("**************************************************DataSourceConfig**************************************************");
     }
 
+//    @Primary
+//    @Bean(name = "testDataSource")
+//    @Qualifier("testDataSource")
+//    @ConfigurationProperties(prefix = "spring.datasource.test")
+//    public DataSource testDataSource() {
+//        return DataSourceBuilder.create().build();
+//    }
+//
+//    @Bean(name = "yoongDataSource")
+//    @Qualifier("yoongDataSource")
+//    @ConfigurationProperties(prefix = "spring.datasource.yoong")
+//    public DataSource yoongDataSource() {
+//        return DataSourceBuilder.create().build();
+//    }
+
+
+    /**
+     * DataSource的两种配置方法
+     * https://blog.csdn.net/newhanzhe/article/details/80763581
+     *
+     * @return
+     */
+    @Primary
+    @Bean(name = "testDataSourceProperties")
+    @Qualifier("testDataSourceProperties")
+    @ConfigurationProperties(prefix = "spring.datasource.test")
+    public DataSourceProperties testDataSourceProperties() {
+        return new DataSourceProperties();
+    }
+
+    @Primary
     @Bean(name = "testDataSource")
     @Qualifier("testDataSource")
-    @Primary
     @ConfigurationProperties(prefix = "spring.datasource.test")
-    public DataSource mysqlDataSource() {
-        return DataSourceBuilder.create().build();
+    public DataSource test2DataSource() {
+        return testDataSourceProperties().initializeDataSourceBuilder().build();
+    }
+
+    @Bean(name = "yoongDataSourceProperties")
+    @Qualifier("yoongDataSourceProperties")
+    @ConfigurationProperties(prefix = "spring.datasource.yoong")
+    public DataSourceProperties yoongDataSourceProperties() {
+        return new DataSourceProperties();
     }
 
     @Bean(name = "yoongDataSource")
     @Qualifier("yoongDataSource")
     @ConfigurationProperties(prefix = "spring.datasource.yoong")
-    public DataSource yoongDataSource() {
-        return DataSourceBuilder.create().build();
+    public DataSource yoong2DataSource() {
+        return yoongDataSourceProperties().initializeDataSourceBuilder().build();
     }
-
 }
