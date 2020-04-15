d59c014c21d9cb3cc28d08e757c4b36bb65dca0c
xiongchao <xiongchao@fincs.com.cn>
2020-04-15 20:28:56 +0800

    redis

--- springboot-community/src/main/java/com/yoong/community/controller/RedisController.java
+++ springboot-community/src/main/java/com/yoong/community/controller/RedisController.java
@@ -2,8 +2,12 @@ package com.yoong.community.controller;
 
 import com.yoong.community.domain.rehearsal.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.core.RedisCallback;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,6 +15,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+/**
+ * Spring Boot 使用NoSQL数据库 Redis
+ * http://www.spring4all.com/article/254
+ */
 @Controller
 @RequestMapping("/redis")
 public class RedisController {
@@ -26,12 +34,15 @@ public class RedisController {
     @Autowired
     private RedisTemplate<String, User> userRedisTemplate;
 
+    //访问Actuator
+    //http://127.0.0.1:8091/actuator/health
+
     /**
-     * Redis测试：http://127.0.0.1:8091/redis/add2Redis
+     * Redis测试：http://127.0.0.1:8090/redis/saveRedis
      */
     @ResponseBody
-    @RequestMapping("/add2Redis")
-    public String redis() {
+    @RequestMapping("/saveRedis")
+    public String saveRedis() {
         try {
             //保存字符串
             String currentDate = dateFormat.format(new Date());
@@ -52,7 +63,35 @@ public class RedisController {
         return "Success";
     }
 
-    //访问Actuator
-    //http://127.0.0.1:8090/actuator/health
+    /**
+     * Redis测试：http://127.0.0.1:8090/redis/saveRedis2
+     */
+    @ResponseBody
+    @RequestMapping("/saveRedis2")
+    public String saveRedis2() {
+        String key = "date";
+        String value = dateFormat.format(new Date());
+        Object result = redisTemplate.execute(new RedisCallback() {
+            @Override
+            public Object doInRedis(RedisConnection redisConnection) throws DataAccessException {
+                RedisSerializer<String> serializer = redisTemplate.getStringSerializer();
+                Boolean innerBool = redisConnection.set(serializer.serialize(key), serializer.serialize(value));
+                return innerBool;
+            }
+        });
+        System.out.println(result);
 
+        Object result02 = redisTemplate.execute(new RedisCallback() {
+            @Override
+            public Object doInRedis(RedisConnection redisConnection) throws DataAccessException {
+                RedisSerializer<String> serializer = redisTemplate.getStringSerializer();
+                byte[] innerBytes = redisConnection.get(serializer.serialize(key));
+                String innerString = serializer.deserialize(innerBytes);
+                return innerString;
+            }
+        });
+        System.out.println(result02);
+
+        return result02.toString();
+    }
 }
